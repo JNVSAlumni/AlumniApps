c61863e5b57ab3d32f379e681ab1fae4518aa589
Bipul Raman <8312486+BipulRaman@users.noreply.github.com>
2022-12-09 23:09:57 +0530

    Code updated

--- MFEApps/src/index.tsx
+++ MFEApps/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { Accounts } from "./components/Accounts";
 
-const accountsElementId = "app";
+const accountsElementId = "jnvapp-accounts";
 try {
   const root = ReactDOM.createRoot(document.getElementById(accountsElementId) as HTMLElement);
   root.render(
